b8fdc81ba5891e6f7a07b329883911b2bee3e4f0
Smgknife <smgknife@gmail.com>
2026-03-04 02:45:19 +0530

    Update configuration values for Discord and Spotify

--- src/config.js
+++ src/config.js
@@ -1,23 +1,24 @@
-
 module.exports = {
-  token: process.env.DISCORD_TOKEN || '', // your discord bot token
-  clientId: process.env.CLIENT_ID || "",
-  prefix: process.env.PREFIX || '!!', // bot prefix
-  ownerID: (process.env.OWNER_IDS || '').split(','), //your discord id
-  SpotifyID: process.env.SPOTIFY_ID || 'e6f84fbec2b44a77bf35a20c5ffa54b8', // spotify client id
-  SpotifySecret: process.env.SPOTIFY_SECRET || '498f461b962443cfaf9539c610e2ea81', // spotify client secret
-  ankushcolor: '#ff0000', // embed colour
+  token:
+    process.env.DISCORD_TOKEN ||
+    "MTQ3ODQ5NjEyODM0ODQ1NDk0Mw.GRU3Sl.iU28hmwLCZBiFvPl1J8eV1A7u3Kfg0JQFCcgAE", // your discord bot token
+  clientId: process.env.CLIENT_ID || "1478496128348454943",
+  prefix: process.env.PREFIX || ",", // bot prefix
+  ownerID: (process.env.OWNER_IDS || "1321883217455874082").split(","), //your discord id
+  SpotifyID: process.env.SPOTIFY_ID || "da88073ec4c14c3b8da27a4071482d51", // spotify client id
+  SpotifySecret:
+    process.env.SPOTIFY_SECRET || "43daf3259ab14b4ab0cc45c98764ac22", // spotify client secret
+  ankushcolor: "#ff0000", // embed colour
   bugReportChannel: "", // ID of the channel where bug reports will be sent
-  embedColor: '#ff0000', // Using your existing ankushcolor
+  embedColor: "#ff0000", // Using your existing ankushcolor
   supportServer: "https://discord.com/invite/w77ymEU82a", // Your support server link
-  
+
   nodes: [
     {
       url: `lavalinkv4.serenetia.com:80`,
       name: `NOTHING`,
       auth: `https://dsc.gg/ajidevserver`,
-      secure: false
-    }
+      secure: false,
+    },
   ],
 };
-
